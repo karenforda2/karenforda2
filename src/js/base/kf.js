efe0dd9c3122e9133e6995b391d12028dedd38bc
Karen Ford <karen.ford@perficient.com>
2017-08-29 23:24:47 -0400

    updated styling, added scrolling animation

--- src/js/base/kf.js
+++ src/js/base/kf.js
@@ -1,12 +1,40 @@
 (!function() {
-	"use strict";
+	'use strict';
 
 	function init() {
-		window.kf = {
-			controls: {},
-			pages: {},
-			utils: {}
+		$('.navigation--item a').on('click', function(e) {
+			var elementID = $(this).attr('href');
+			e.preventDefault();
+			scrollToCoord(elementID);
+		});
+	}
+
+	function scrollToCoord(element) {
+		var scrollInt = null;
+		var elPosition = $(element).offset().top - 20;
+		var body = document.body;
+		var	html = document.documentElement;
+		var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+		// if element is short, and the document cannot be scrolled to the actual element, then set scroll position to scroll as low as is possible
+		if (elPosition + window.innerHeight > docHeight) {
+			elPosition = docHeight - window.innerHeight;
 		}
+
+		// continue scroll incrementally, until reaching the final element position
+		scrollInt = window.setInterval(function() {
+			var curY = window.scrollY || window.pageYOffset;
+			var step = Math.ceil((elPosition - curY) / 10);			
+
+			if (curY < elPosition) {
+				window.scrollTo(0, curY + step);
+
+			} else {
+				window.scrollTo(0, elPosition);
+				window.clearInterval(scrollInt);
+				scrollInt = null;
+			}
+		}, 16);
 	}
 
 	init();
